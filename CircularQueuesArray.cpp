e2129c00ba2ec27c39afb12f7fe5d688a89df891
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-28 18:08:05 +0700

    membuat void insert

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -13,3 +13,9 @@ public:
         FRONT = -1;
         REAR = -1;
     }
+    void insert()
+    {
+          int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
